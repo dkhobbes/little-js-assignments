782e6320731112ccc720daf83bc5c0eea4a7156a
David Kurth <david-kurth@sbcglobal.net>
2016-11-16 16:56:43 -0600

    consilidating

---
+++ js-labs/functions/functions-lab-03.js
@@ -0,0 +1,13 @@
+//function goes here
+function sum(numA, numB){
+  var a = numA + numB;
+  return a;
+}
+
+var result = sum(1,9);
+console.log(result);
+
+
+/*
+Write a function that takes two parameters then computes and returns the sum
+*/
---
+++ js-labs/functions/functions-lab-05.js
@@ -0,0 +1,18 @@
+//function goes here
+function shortstring(a, b){
+
+  if (a.length < b.length) {
+    return a;
+  }
+  else {
+    return b;
+  }
+
+}
+
+shortstring("one", "second")
+
+
+/*
+Write a function that takes two strings and returns the longest. If the two are of the same length, it can return either.
+*/
