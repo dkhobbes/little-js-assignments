4fd2c817641ea6aaf8ac11a626d180a76bf213dc
David Kurth <david-kurth@sbcglobal.net>
2016-11-20 16:09:45 -0600

    loops lab 2 done

--- js-labs/loops/loops-lab-02.js
+++ js-labs/loops/loops-lab-02.js
@@ -7,15 +7,13 @@ console.log(nums[2]);
 console.log(nums[3]);
 console.log(nums[4]);*/
 
-var cars = ["Mazda", "Honda", "Mercury", "Ford"];
+var nums = [1, 6, 42, 3, 9];
 var i = 0;
-var text = "";
-
-while (cars[i]) {
-    text += cars[i] + "<br>";
-    i++;
+while ( i < nums.length) {
+    console.log(nums[i]); i++;
 }
-console.log(cars);
+
+
 /*
 Write a while loop and console.log the number out of the array. So there should be five logs each, each with those values. So...
 Expected output:
