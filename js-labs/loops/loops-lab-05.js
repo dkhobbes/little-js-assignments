512e0300028a373fb89caa295deb61f2be56254a
David Kurth <david-kurth@sbcglobal.net>
2016-11-20 17:02:41 -0600

    loop 5 is done

--- js-labs/loops/loops-lab-05.js
+++ js-labs/loops/loops-lab-05.js
@@ -1,8 +1,16 @@
 var nums = [1, 6, 42, 3, 17];
 var bigNums = [];
+var i = 0;
 
 //loop goes here
 
+while(i < nums.length){
+  if(nums[i] > 10){
+    bigNums.push(nums[i])
+
+  };
+  i++;
+}
 console.log(bigNums);
 
 /*
