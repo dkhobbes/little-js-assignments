ca658464385a94b899988ba447ff5ed8d1821f29
David Kurth <david-kurth@sbcglobal.net>
2016-12-18 17:27:33 -0600

    finally fixed js error

--- day-13/office.js
+++ day-13/office.js
@@ -1,22 +1,19 @@
-var fs = require('fs');
+var fs = require("fs");
 
-
-fs.readFile('Import_User_Sample_en.csv', 'utf-8', function(err,data){
+fs.readFile("Import_User_Sample_en.csv", "utf-8", function(err,data){
   var parsed = {
-    UserNames = []
-
+    UserNames: []
   }
-}
 
-var lines = data.split("\n");
-for (var = 1; i < lines.length; i++){
+var lines = data.split("\r\n");
+for (var i = 1; i < lines.length; i++){
   var indivArr = lines[i].split(",");
 
   var userData = {
     UserName: indivArr[0],
-    DisplayName = [3]
-    }
-  }
+    DisplayName: indivArr[3]
+  };
+
   parsed.UserNames.push(userData)
 
 }
